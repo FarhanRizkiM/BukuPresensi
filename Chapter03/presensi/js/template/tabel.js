929e7e7851fe8de6218f26953a666ee81045afb2
Farhan Rizki <farhanrizki101010@gmail.com>
2023-07-24 21:33:13 +0700

    add file

---
+++ Chapter03/presensi/js/template/tabel.js
@@ -0,0 +1,27 @@
+export let rowtabel = `<tr>
+<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+  <div class="flex items-center">
+    <div class="flex-shrink-0 h-10 w-10">
+      <img class="h-10 w-10 rounded-full"
+      #SVG#
+    </div>
+    <div class="ml-4">
+      <div class="text-sm leading-5 font-medium text-gray-900">#nama#</div>
+      <div class="text-sm leading-5 text-gray-500">#prodi_kampus</div>
+    </div>
+  </div>
+</td>
+<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+  <div class="text-sm leading-5 text-gray-900">#npm#</div>
+</td>
+<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+<div class="text-sm leading-5 text-gray-900">#semester#</div>
+</td>
+<td
+  class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
+  #kelas#</td>
+</tr>`
+
+export let smt = `<th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-#col#-500 text-left">#semester#</th>`
+
+export let svg = `<div class="flex items-center justify-center w-10 h-10 text-base font-medium text-yellow-600 bg-#WARNA#-200 rounded-md">PS</div>`
